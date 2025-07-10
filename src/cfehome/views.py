9b322740231d95715e5a0dc29cd3a0ec00e5533e
MinKhantKyaw <minkhant050603@gmail.com>
2025-07-11 00:24:40 +0700

    Track and display page visit counts
    - Modified home_page_view to record each visit in the PageVisit table
    - Queried total visits and visits per page using Django ORM
    - Calculated percentage of total visits per page
    - Passed visit statistics to the template context

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,25 +1,38 @@
+from visits.models import PageVisit
 from django.http import HttpResponse
 # django way
 from django.shortcuts import render
 # python way
 import pathlib
 
+
 this_dir = pathlib.Path(__file__).resolve().parent
+
+# import the model
 
 
 def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "My Page"
     my_context = {
-        "page_title": my_title
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": (page_qs.count() * 100.0) / qs.count(),
+        "total_visit_count": qs.count(),
     }
+    # path = request.path
+    # print("path", path)
     html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
 
 def my_old_home_page_view(request, *args, **kwargs):
     my_title = "My Page"
     my_context = {
-        "page_title": my_title
+        "page_title": my_title,
+
     }
     html_ = """
         <!DOCTYPE html>
