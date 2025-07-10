4f6cc3049928f1263d00119176f1633228050fb7
MinKhantKyaw <minkhant050603@gmail.com>
2025-07-10 22:02:19 +0700

    Serve HTML from external home.html file -- the python way
    - Updated home_page_view to read HTML content from a separate home.html file.
    - Used pathlib to locate the file relative to the view.
    - This separates presentation (HTML) from Python code.

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+# python way
+import pathlib
+
+this_dir = pathlib.Path(__file__).resolve().parent
 
 
 def home_page_view(request, *args, **kwargs):
-    return HttpResponse("<h1>HelloWorld</h1>")
+    html = ""
+    html_file_path = this_dir / "home.html"
+    html = html_file_path.read_text()
+    return HttpResponse(html)
