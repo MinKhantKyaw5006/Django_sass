acee077ebb7b477a9ed9dbc55a3b055f2322671b
MinKhantKyaw <minkhant050603@gmail.com>
2025-07-10 22:34:50 +0700

    Update TEMPLATES setting to include custom template directories
    - Added `templates/` and `cfehome/` to TEMPLATES['DIRS'].
    - Ensures Django searches both global and app-level folders for templates.
    - Retains APP_DIRS=True for app templates.

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+# django way
+from django.shortcuts import render
 # python way
 import pathlib
 
@@ -6,7 +8,27 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 
 def home_page_view(request, *args, **kwargs):
-    html = ""
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_template = "home.html"
+    return render(request, html_template)
+
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <h1>{page_title}</h1>
+            </body>
+        </html>
+""".format(**my_context)  # page_title = my_title
     html_file_path = this_dir / "home.html"
     html = html_file_path.read_text()
     return HttpResponse(html)
